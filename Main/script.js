7dc77d13c6b2905e31d7865947d2b5d1771512e6
Harshit Awasthi <awasthiharshit8@gmail.com>
2023-07-12 14:21:52 +0530

    first commit

---
+++ Main/script.js
@@ -0,0 +1,17 @@
+let ulTasks = $('#ulTasks')
+let btnAdd = $('#ulTasks')
+let btnClear = $('#ulTasks')
+let inpNewTask = $('#ulTasks')
+
+
+
+btnAdd.click(()=>
+
+{
+
+    console.log(inpNewTask.val())
+    inpNewTask.val("")
+
+}
+
+)
